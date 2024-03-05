58a3b5393c9c2f76df92fbdadf6dc1d52a2f1c50
vahid_mirzaei <vahidito@gmail.com>
2024-03-05 12:45:31 +0330

    first attempt to test commit and push

---
+++ Data Scraping.py
@@ -0,0 +1,13 @@
+# Data Scraping
+
+## Session_1
+### Introduction
+
+#_____________________________________________________________________________________________________________________
+
+## Session_2
+###_Install_Libraries
+
+import numpy as np
+a = np.sqrt(4)
+print(a)
