af411cd3a19b8c17a995393d57e8d87ce124df25
Kimiagar <vahidito@gmail.com>
2024-03-07 16:31:08 +0330

    explore request package

--- Data Scraping.py
+++ Data Scraping.py
@@ -8,6 +8,39 @@
 ## Session_2
 ###_Install_Libraries
 
-# in this session we installed the requests and beautifulsoup packages
+# in this session we installed the requests and Beautifulsoup packages
 
+# _____________________________________________________________________________________________________________________
+
+## Session_3
+###_Exploring the requests package
+
+import requests
+
+url = "https://www.python.org/"
+response = requests.get(url)
+
+print(response.request.headers)
+print(type(response.request.headers))
+print(response.status_code)
+print(response.reason)
+print(response.headers)
+print(response.text)  # source code
+url_1 = "https://www.python.org/static/img/python-logo.png"
+respo = requests.get(url_1)
+with open("1.jpg", 'wb') as r:
+    r.write(respo.content)
+
+# _____________________________________________________________________________________________________________________
+
+## Session_4
+###_Exploring the BeautifulSoup package
+
+
+
+
+
+
+
+# _____________________________________________________________________________________________________________________
 
