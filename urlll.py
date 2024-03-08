83f377181af6e9a1e1316a910ac5b9b167de1588
Kimiagar <vahidito@gmail.com>
2024-03-08 20:59:32 +0330

    practise

--- urlll.py
+++ urlll.py
@@ -2,14 +2,18 @@ import requests
 from bs4 import BeautifulSoup as bs
 import numpy as np
 import pandas as pd
-url = 'http://old.tsetmc.com/Loader.aspx?ParTree=15'
+url = 'https://www.varzesh3.com/'
 response = requests.get(url)
 soup = bs(response.text, 'html.parser')
-print(soup.text)
+# print(soup.text)
 
-# table_3 = soup_3.find('table')
-# rows = table_3.find_all('tr')
-# print(rows)
+
+url_3 = 'https://www.skysports.com/premier-league-table'
+response_3 = requests.get(url_3)
+soup_3 = bs(response_3.text, 'html.parser')
+table_3 = soup_3.find('table')
+rows = table_3.find_all('tr')
+print(rows)
 # for row in rows:
 #     data = []
 #     for head in row.findAll('th'):
